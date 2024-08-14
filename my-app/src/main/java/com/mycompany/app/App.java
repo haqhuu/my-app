68b3a96c9630ef3ed75b4cb15f9723d1ee7c5d5b
haqhuu <115322147+haqhuu@users.noreply.github.com>
2024-08-14 21:22:20 +0700

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,6 +6,6 @@ package com.mycompany.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("--->Check APP console: Hello World!");
     }
 }
